914a50e60f4f059e80b0d9090c794c0d9b4e01de
hisashito <121297410+hisashito@users.noreply.github.com>
2024-10-29 18:31:12 -0400

    Update Cinema.java etapa 2
    Se agrega lógica para calcular el precio de los boletos según el número de filas y asientos. (que varía también de acuerdo a su ubicación)

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Cinema {
+
     public static void main(String[] args) {
         int rows = 7;
         int cols = 8;
+        rows = askForInt("enter the number of rows:");
+        cols = askForInt("Enter the number of seats in each row:");
+        System.out.println("Total income:" + System.lineSeparator() + "$" + calculateTotalIncome(rows,cols));
+    }
+
+    public static void displaySeats (int rows, int cols) {
         String seat = "S";
         String row = seat.repeat(cols);
-        System.out.println("Cinema:");
-        
+        System.out.println("Cinema:");    
         String toPrint = " ";
             for (int i = 0 ; i < rows + 1; i++) {
                 for (int j = 0; j < cols + 1; j++) {
@@ -22,5 +30,21 @@ public class Cinema {
                 }
                 System.out.println();
             }
+    }
+
+    public static int calculateTotalIncome (int rows, int cols) {
+        int seats = rows * cols;
+        int price1 = 10;
+        int price2 = 8;
+        
+        if (seats > 60){
+            return cols * ((rows / 2) * (price1 - price2) + rows * price2);
+        }
+        return seats * price1;
+    }
+    
+    public static int askForInt(String in){
+        System.out.println(in);
+        return (new Scanner(System.in)).nextInt();
     }
 }
