c63f731de5605d9ee1c87dce71b65df824db0292
hisashito <121297410+hisashito@users.noreply.github.com>
2024-10-29 20:07:13 -0400

    Update Cinema.java etapa 3 completa
    se agregó funcion para preguntar por el precio de algún asiento

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,33 +1,37 @@
-/**
-En esta etapa, se deben leer dos números enteros positivos: el número de filas y el número de asientos por fila. 
-
-El precio de los boletos se determina según las siguientes reglas:
-
-Si el total de asientos no supera 60, cada boleto cuesta 10 dólares.
-En salas más grandes, los boletos cuestan 10 dólares para la mitad frontal de las filas y 8 dólares para 
-la mitad trasera. 
-Si el número de filas es impar (por ejemplo, 9), la primera mitad incluye las primeras 4 filas y la segunda 
-mitad abarca las 5 filas restantes.
-Se debe calcular la ganancia por los boletos vendidos según el número de asientos y mostrar el resultado, 
-deteniendo luego el programa. 
-El número de filas y asientos no excederá 9.
+/*
+Leer dos números enteros positivos que representen el número de filas y el número de asientos por fila.
+Imprimir el arreglo de asientos según el formato establecido en la etapa anterior.
+Leer dos números enteros que representen el número de fila y el número de asiento seleccionados.
+Determinar el precio del boleto basado en las reglas previamente establecidas:
+10 dólares si el total de asientos es 60 o menos.
+10 dólares para la mitad frontal de las filas y 8 dólares para la mitad trasera en salas más grandes.
+Imprimir todos los asientos en la sala de cine, marcando el asiento elegido con el símbolo "B".
+Mostrar el precio del boleto y detener el programa.
 */
 
-import java.util.Scanner;
-
 public class Cinema {
+
+    static int price1 = 10;
+    static int price2 = 8;
+    static int nSeats = 60;
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         int rows = 7;
         int cols = 8;
-        rows = askForInt("enter the number of rows:");
+        rows = askForInt("Enter the number of rows:");
         cols = askForInt("Enter the number of seats in each row:");
-        System.out.println("Total income:" + System.lineSeparator() + "$" + calculateTotalIncome(rows,cols));
-    }
-
-    public static void displaySeats (int rows, int cols) {
+        displaySeats(rows, cols, -1, -1, false);
+        int seatRow = askForInt("Enter a row number:");
+        int seatCol = askForInt("Enter a seat number in that row:");
+        int price = checkPrice(rows, cols, seatRow);
+        System.out.println("Ticket price: $%d ".formatted(price));
+        displaySeats(rows, cols, seatRow, seatCol, true);
+   }
+    
+    public static void displaySeats (int rows, int cols, int row, int col, boolean book) {
         String seat = "S";
-        String row = seat.repeat(cols);
+        //String row = seat.repeat(cols);
         System.out.println("Cinema:");    
         String toPrint = " ";
             for (int i = 0 ; i < rows + 1; i++) {
@@ -37,7 +41,7 @@ public class Cinema {
                     } else if (i > 0 && j == 0) {
                         toPrint = i + " "; 
                     } else if ( i > 0 && j > 0) {
-                        toPrint = seat + " ";
+                        toPrint = (book && i == row && j == col) ? "B ":seat + " "; 
                     } else if (i == 0 && j == 0) {
                         toPrint = "  ";
                     }
@@ -49,9 +53,6 @@ public class Cinema {
 
     public static int calculateTotalIncome (int rows, int cols) {
         int seats = rows * cols;
-        int price1 = 10;
-        int price2 = 8;
-        
         if (seats > 60){
             return cols * ((rows / 2) * (price1 - price2) + rows * price2);
         }
@@ -60,6 +61,14 @@ public class Cinema {
     
     public static int askForInt(String in){
         System.out.println(in);
-        return (new Scanner(System.in)).nextInt();
+        return sc.nextInt();
+    }
+
+    public static int checkPrice(int rows, int cols, int row) {
+        return hasManySeats(rows *  cols) &&  row > rows / 2 ? price2 : price1;                    
+    }
+
+    public static boolean hasManySeats(int seats){
+        return seats > nSeats;
     }
 }
